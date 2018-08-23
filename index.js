cb8ea6426e8cd5c9b85c56c55794b11a310384d5
asia <asiaelyse@gmail.com>
2018-08-23 16:49:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // write your code below!
 
-happyHolidays()
+function happyHolidays() {
   return 'Happy holidays!'
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
